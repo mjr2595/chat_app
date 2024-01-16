97ff23c895a8dc18e335d84309001e6a4fdd9703
mjr2595 <michaeljohnraymond@gmail.com>
2024-01-15 21:18:52 -0600

    Add sign out functionality to app bar

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:firebase_auth/firebase_auth.dart';
+
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
 
@@ -8,6 +10,17 @@ class ChatScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('FlutterChat'),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.exit_to_app,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+          ),
+        ],
       ),
       body: const Center(
         child: Text('Logged In!'),
